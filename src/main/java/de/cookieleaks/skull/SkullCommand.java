833c821bd63ddc1669c2cb46195212e99a53b481
KeksGauner <keksgauner@gmail.com>
2023-08-01 18:36:54 +0200

    fix: some stuff

--- src/main/java/de/cookieleaks/skull/SkullCommand.java
+++ src/main/java/de/cookieleaks/skull/SkullCommand.java
@@ -9,6 +9,8 @@ import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -18,14 +20,31 @@ public class SkullCommand extends BukkitCommand {
         super("skull");
     }
 
-    static HashMap<UUID, Long> cooldowns = new HashMap<>();
+    static HashMap<UUID, LocalDateTime> cooldowns = new HashMap<>();
 
     @Override
     public boolean execute(@NotNull CommandSender sender, @NotNull String label, @NotNull String[] strings) {
         if (sender instanceof Player player) {
             if (cooldowns.containsKey(player.getUniqueId())) {
-                long secondsLeft = ((cooldowns.get(player.getUniqueId()) / 1000) + 86400) - (System.currentTimeMillis() / 1000);
-                player.sendMessage("§cDu kannst deinen Kopf erst in " + secondsLeft + " Sekunden wieder erhalten!");
+                Duration remainingTime = Duration.between(LocalDateTime.now(), cooldowns.get(player.getUniqueId()));
+
+                long hours = remainingTime.toHours() % 24;
+                long minutes = remainingTime.toMinutes() % 60;
+                long seconds = remainingTime.getSeconds() % 60;
+                player.sendMessage("§cDu kannst deinen Kopf erst in " + hours + " Stunden " + minutes + " Minuten und " + seconds + " Sekunden wieder erhalten!");
+                return false;
+            }
+
+            // check if enough space
+            int freeSlots = 0;
+            for (ItemStack item : player.getInventory().getContents()) {
+                if (item == null) {
+                    freeSlots++;
+                }
+            }
+
+            if (freeSlots < 1) {
+                player.sendMessage("§cDu hast nicht genug Platz im Inventar!");
                 return false;
             }
 
@@ -43,7 +62,7 @@ public class SkullCommand extends BukkitCommand {
     }
 
     public void addTimer(Player player) {
-        cooldowns.put(player.getUniqueId(), System.currentTimeMillis());
+        cooldowns.put(player.getUniqueId(), LocalDateTime.now().plusHours(24));
         new BukkitRunnable() {
             @Override
             public void run() {
